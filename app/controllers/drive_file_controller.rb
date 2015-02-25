c2b2ba6a8a91507a154648948ddc6125ee47168b
Gabriel Guilbart <gabriel.guilbart@gmail.com>
2015-02-25 21:15:40 +0100

    GoogleDrive gem file to spreadsheet handling

--- app/controllers/drive_file_controller.rb
+++ app/controllers/drive_file_controller.rb
@@ -27,7 +27,8 @@ class DriveFileController < ApplicationController
   		#	api_method: drive.files.get,
   		#	parameters: {fileId: state['ids'][0]}
   		#)
-  		@file = session.file_by_id(state['exportIds'][0])
+  		file = session.file_by_id(state['exportIds'][0])
+  		@spreadsheet = session.spreadsheet_by_title(file.title)
   	end
   end
 end
